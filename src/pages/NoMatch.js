e32ed901ab0ab807ded0c3a5fb440fc4e0139d94
Johj <peter.han.9493@gmail.com>
2017-07-19 16:18:27 -0700

    Update 404 page

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,11 +1,40 @@
 import React, { Component, } from 'react';
-import { Panel, } from 'react-bootstrap';
+import {
+  Col,
+  Grid,
+  Image,
+  Panel,
+  Row,
+} from 'react-bootstrap';
+ 
+import { imagePath, } from '../util/imagePath';
 
 export default class NoMatch extends Component {
   render = () => {
     return (
       <Panel>
-        404
+        <Grid fluid>
+          <Row style={{display: 'flex', alignItems: 'center',}}>
+            <Col lg={2} md={2} smHidden xsHidden></Col>
+            <Col lg={4} md={4} sm={12} xs={12}>
+              <img alt='' src={imagePath('cqdb', 'public/favicon.png')} />
+              <p />
+              <p>
+                <strong>404.</strong>
+                <text style={{color: '#777'}}> Oops! My hands slipped!</text>
+              </p>
+              <p />
+              <p>
+                {`The requested URL ${window.location.pathname} was not found on this server.`}
+                <text style={{color: '#777'}}> That's all we know.</text>
+              </p>
+            </Col>
+            <Col lg={4} md={4} smHidden xsHidden>
+              <Image alt='' responsive src={imagePath('cqdb', 'src/assets/fergus_cutout.png')} />
+            </Col>
+            <Col lg={2} md={2} smHidden xsHidden></Col>
+          </Row>
+        </Grid>
       </Panel>
     );
   }
