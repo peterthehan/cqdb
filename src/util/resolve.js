12af193bbe6c4957454bbfc3eb4e3a5a318cc152
Johj <peter.han.9493@gmail.com>
2017-07-22 16:21:37 -0700

    Show failing output

--- src/util/resolve.js
+++ src/util/resolve.js
@@ -2,5 +2,5 @@ const data = require('../Decrypted/filtered_textlocale_en_us.json');
 
 export function resolve(value) {
   //console.log(value);
-  return !data[value] ? '' : data[value];
+  return data[value] === null ? 'null' : data[value];
 }
