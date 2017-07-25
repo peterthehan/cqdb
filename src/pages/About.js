764565dd8d6dc5ece150484601994d72817bf895
Johj <peter.han.9493@gmail.com>
2017-07-25 03:57:25 -0700

    Remove dependencies panel

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,21 +7,7 @@ import {
   Row,
 } from 'react-bootstrap';
 
-const dependencies = {
-  'bootstrap: 3.3.7': 'https://www.npmjs.com/package/bootstrap',
-  'react: 15.6.1': 'https://www.npmjs.com/package/react',
-  'react-bootstrap: 0.31.0': 'https://www.npmjs.com/package/react-bootstrap',
-  'react-dom: 15.6.1': 'https://www.npmjs.com/package/react-dom',
-  'react-list: 0.8.6': 'https://www.npmjs.com/package/react-list',
-  'react-router-bootstrap: 0.24.2': 'https://www.npmjs.com/package/react-router-bootstrap',
-  'react-router-dom: 4.1.1': 'https://www.npmjs.com/package/react-router-dom',
-};
-
 export default class About extends Component {
-  renderDependencies = (i) => {
-    return <a href={dependencies[i]} key={i}>{i}<br /></a>;
-  }
-
   render = () => {
     return (
       <Row>
@@ -31,6 +17,7 @@ export default class About extends Component {
               <Media.Body>
                 <Media.Heading>Contacts</Media.Heading>
                 <p>Discord: Miku#0039<br />Crusaders Quest: Saarja</p>
+                <a href='https://github.com/Johj/cqdb'>https://github.com/Johj/cqdb</a>
               </Media.Body>
               <Media.Right>
                 <Image alt='' rounded src='https://avatars6.githubusercontent.com/u/16639331?v=4&s=460' width='128' />
@@ -39,12 +26,6 @@ export default class About extends Component {
           </Panel>
         </Col>
         <Col lg={6} md={6} sm={12} xs={12}>
-          <Panel>
-            <Media.Heading>Dependencies</Media.Heading>
-            {Object.keys(dependencies).map(this.renderDependencies)}
-          </Panel>
-        </Col>
-        <Col lg={12} md={12} sm={12} xs={12}>
           <Panel>
             <Media.Heading>Disclaimer</Media.Heading>
             <p>
