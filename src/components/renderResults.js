6f07048233833b804e16581fdcb370190a66ace5
Johj <peter.han.9493@gmail.com>
2017-07-27 16:12:44 -0700

    Increase height to 100vh

--- src/components/renderResults.js
+++ src/components/renderResults.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactList from 'react-list';
 import {
+  Col,
   ListGroup,
   Panel,
 } from 'react-bootstrap';
 
 export function renderResults(title, render) {
   return (
-    <Panel collapsible defaultExpanded header={`${title} (${render.length})`}>
-      <ListGroup fill style={{maxHeight: '65vh', overflow: 'auto',}}>
-        <ReactList
-          itemRenderer={i => render[i]}
-          length={render.length}
-          minSize={10}
-        />
-      </ListGroup>
-    </Panel>
+    <Col lg={12} md={12} sm={12} xs={12}>
+      <Panel collapsible defaultExpanded header={`${title} (${render.length})`}>
+        <ListGroup fill style={{maxHeight: '100vh', overflow: 'auto',}}>
+          <ReactList
+            itemRenderer={i => render[i]}
+            length={render.length}
+            minSize={10}
+          />
+        </ListGroup>
+      </Panel>
+    </Col>
   );
 }
