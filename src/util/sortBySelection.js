8828284121ed9bbd912453af969b2f8c2dcf7519
Johj <peter.han.9493@gmail.com>
2017-07-28 16:36:23 -0700

    Only clone if mutating

--- src/util/sortBySelection.js
+++ src/util/sortBySelection.js
@@ -1,22 +1,22 @@
 export function sortBySelection(filtered, sortBy, sortIsDescending, selectsDefault) {
+  if (sortBy === selectsDefault && sortIsDescending) {
+    return filtered; // return as-is if default
+  }
+
   const cloned = [...filtered];
   
   if (sortBy === selectsDefault) {
-    if (!sortIsDescending) {
-      cloned.reverse();
-    }
+    cloned.reverse();
+  } else if (sortIsDescending) {
+    cloned.sort((a, b) => {
+      if (a.sortable[sortBy] === b.sortable[sortBy]) { return 0; }
+      return a.sortable[sortBy] > b.sortable[sortBy] ? -1 : 1;
+    });
   } else {
-    if (sortIsDescending) {
-      cloned.sort((a, b) => {
-        if (a.sortable[sortBy] === b.sortable[sortBy]) { return 0; }
-        return a.sortable[sortBy] > b.sortable[sortBy] ? -1 : 1;
-      });
-    } else {
-      cloned.sort((a, b) => {
-        if (a.sortable[sortBy] === b.sortable[sortBy]) { return 0; }
-        return a.sortable[sortBy] < b.sortable[sortBy] ? -1 : 1;
-      });
-    }
+    cloned.sort((a, b) => {
+      if (a.sortable[sortBy] === b.sortable[sortBy]) { return 0; }
+      return a.sortable[sortBy] < b.sortable[sortBy] ? -1 : 1;
+    });
   }
 
   return cloned;
