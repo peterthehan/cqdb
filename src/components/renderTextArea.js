f44d884a806c4937723335d7890aadbec8238990
Johj <peter.han.9493@gmail.com>
2017-07-27 20:42:04 -0700

    Add magnifying glass for ux

--- src/components/renderTextArea.js
+++ src/components/renderTextArea.js
@@ -3,19 +3,25 @@ import {
   Col,
   FormControl,
   FormGroup,
+  Glyphicon,
+  InputGroup,
 } from 'react-bootstrap';
 
 export function renderTextArea(handleTextChange, textFilter) {
   return (
     <Col lg={8} md={8} sm={12} xs={12}>
       <FormGroup style={{marginBottom: '5px',}}>
-        <FormControl
-          componentClass='textarea'
-          onChange={handleTextChange}
-          placeholder='Name'
-          style={{height: '34px', resize: 'none',}}
-          value={textFilter}
-        />
+        <InputGroup>
+          <FormControl
+            onChange={handleTextChange}
+            placeholder='Name'
+            type='text'
+            value={textFilter}
+          />
+          <InputGroup.Addon>
+            <Glyphicon glyph='search' />
+          </InputGroup.Addon>
+        </InputGroup>
       </FormGroup>
     </Col>
   );
