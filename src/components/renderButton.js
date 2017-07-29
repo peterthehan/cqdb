c39421f9bc6e04b5ed5ee8ea9e89306767b32207
Johj <peter.han.9493@gmail.com>
2017-07-28 17:11:40 -0700

    Remove col from modal, fix spacing between result and query

--- src/components/renderButton.js
+++ src/components/renderButton.js
@@ -7,7 +7,7 @@ import {
 export function renderButton(handleButton, label) {
   return (
     <Col lg={4} md={4} sm={12} xs={12}>
-      <Button block onClick={handleButton}>
+      <Button block onClick={handleButton} style={{marginBottom: 5,}}>
         {label}
       </Button>
     </Col>
