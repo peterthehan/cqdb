d2bb0e34481a622e22c2150e5f78f9ab6f90145a
Johj <peter.han.9493@gmail.com>
2017-09-14 01:42:33 -0700

    Resolve edge case with bad keys from data

--- src/pages/Interactions.js
+++ src/pages/Interactions.js
@@ -24,20 +24,38 @@ const data = interactionData.map(i => {
   for (let j of Object.keys(i.eatereggherotext)) {
     let d;
 
+    // handle edge case involving bad key names in interactions data file
+    const dict = {
+      'COS_PR_6_2': 'TEXT_CHA_PR_2_6', // summer vacation maria
+      'COS_EVENT_AR_14_OLYMPIC': 'TEXT_COS_AR_14_OLYMPIC', // mamba of the national team
+      'COS_WI_5_10': 'TEXT_COS_EVENT_WI_5_WINTER', // ice queen lilith
+      'COS_AR_4_17_ROCK': 'TEXT_COS_AR_4_17_NAME', // devastating drum robin hood
+      'COS_EVENT_WI_11_OLYMPIC': 'TEXT_COS_WI_11_OLYMPIC', // samba cano
+      'COS_EVENT_WI_5_OLYMPIC': 'TEXT_COS_WI_5_OLYMPIC', // carnival lilith
+      'COS_EVENT_WA_5_LABORDAY': 'TEXT_COS_EVENT_WA_5_LAVORDAY_NAME', // monte98
+      'COS_WA_2_2_RE': 'TEXT_COS_WA_2_2', // joan, knight of roses (redesign)
+    };
+
     const flag = j.startsWith('CHA');
     const hero = flag
       ? heroData[heroData.findIndex(k => k.id === j)]
-      : skinData[skinData.findIndex(k => k.costume_name === `TEXT_${j}${k.costume_name.endsWith('_NAME') ? '_NAME' : ''}`)];
+      : skinData[skinData.findIndex(k => {
+          const currentSkin = j in dict
+            ? dict[j]
+            : `TEXT_${j}${k.costume_name.endsWith('_NAME') ? '_NAME' : ''}`; 
+          return k.costume_name === currentSkin;
+        })];
 
     if (hero == null) {
-      // console.log(j, hero);
+      // console.log(j, hero, i.id);
       continue;
     }
 
+    // edge case with joan skin
     d = {
       name: resolve(flag ? hero.name : hero.costume_name),
       dialogue: resolve(i.eatereggherotext[j]),
-      image: imagePath('cq-assets', `${flag ? 'heroes' : 'skins'}/${hero.face_tex}.png`),
+      image: imagePath('cq-assets', `${flag ? 'heroes' : 'skins'}/${hero.face_tex}${j === 'COS_WA_2_2_RE' ? '_re' : ''}.png`),
     }
 
     dialogues.push(d);
