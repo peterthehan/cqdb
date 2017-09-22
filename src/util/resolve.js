b17c719ca8b5881bda0f0b3906ea97764bfe2715
Johj <peter.han.9493@gmail.com>
2017-09-22 01:19:59 -0700

    Minor comment change for consistency

--- src/util/resolve.js
+++ src/util/resolve.js
@@ -1,6 +1,9 @@
-const data = require('../Decrypted/filtered_textlocale_en_us.json');
+const real = require('../Decrypted/filtered_textlocale_en_us.json');
+const frivolous = require('../Decrypted/frivolous_textlocale_en_us.json');
+
+const data = Object.assign(real, frivolous);
 
 export function resolve(value) {
-  //console.log(value);
+  // console.log(value);
   return value in data ? data[value] : value;
 }
