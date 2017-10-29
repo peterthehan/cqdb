4664fb0562867c246b47ef81c50fb694caf68207
Johj <peter.han.9493@gmail.com>
2017-10-29 01:56:22 -0700

    Refactor home, move discord links to links page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { Component, } from 'react';
 import {
   Col,
-  Image,
   Media,
   Panel,
   Row,
@@ -13,47 +12,16 @@ export default class Home extends Component {
   render = () => {
     return (
       <Row>
-        <Col 
+        <Col
           style={{alignItems: 'center', display: 'flex', justifyContent: 'center',}}
           lg={12} md={12} sm={12} xs={12}
         >
-          <Image alt='' responsive src={imagePath('src/assets/banner', 'cqdb')} />
+          <img alt='' src={imagePath('src/assets/banner', 'cqdb')} style={{maxWidth: '100%',}} />
         </Col>
         <Col lg={12} md={12} sm={12} xs={12}>
           <Panel>
             <Media.Heading>Hello!</Media.Heading>
-            Welcome to the Crusaders Quest Database. Here you will find all kinds of useful and up-to-date information regarding your favorite game. Enjoy your stay!
-          </Panel>
-        </Col>
-        <Col lg={6} md={6} sm={12} xs={12}>
-          <Panel>
-            <Media.Heading>Join the Discussion!</Media.Heading>
-            <Row>
-              <Col
-                style={{display: 'flex', justifyContent: 'center',}}
-                lg={12} md={12} sm={12} xs={12}
-              >
-                <a href='https://discord.gg/WjEFnzC'>
-                  <Image alt='cqdb Discord Server' responsive src='https://discordapp.com/api/guilds/258167954913361930/embed.png?style=banner2' />
-                </a>
-              </Col>
-              <Col lg={12} md={12} sm={12} xs={12}><p /></Col>
-              <Col
-                style={{display: 'flex', justifyContent: 'center',}}
-                lg={12} md={12} sm={12} xs={12}
-              >
-                <a href='https://discord.gg/6TRnyhj'>
-                  <Image alt='Crusaders Quest Discord Server' responsive src='https://discordapp.com/api/guilds/206599473282023424/embed.png?style=banner2' />
-                </a>
-              </Col>
-            </Row>
-          </Panel>
-        </Col>
-        <Col lg={6} md={6} sm={12} xs={12}>
-          <Panel>
-            <Media.Heading>Fergus</Media.Heading>
-            <a href='https://github.com/Johj/fergus'>Fergus</a> is a Discord bot for Crusaders Quest. Enjoy all the features of cqdb inside your Discord server!
-            Bot invitation link can be found <a href='https://goo.gl/nDluCQ'>here</a>.
+            Welcome to the Crusaders Quest Database.
           </Panel>
         </Col>
       </Row>
